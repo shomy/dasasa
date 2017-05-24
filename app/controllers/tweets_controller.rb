c32513967c528cdeb71fb1d9342a7df5ba97052a
shohei <sksks0607@gmail.com>
2017-05-24 14:44:20 +0000

    lo

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,57 @@
+class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.all
+  end
+
+
+  def new
+    if params[:back]
+      @tweet = Tweet.new(tweets_params)
+    else
+      @tweet = Tweet.new
+    end
+  end
+
+  def create
+    @tweet = Tweet.new(tweets_params)
+    if @tweet.save
+      redirect_to root_path, notice:"ツイート！！"
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+  
+  def update
+   @tweet = Tweet.find(params[:id])
+   if @tweet.update(tweets_params) 
+     redirect_to root_path
+   else
+     render'edit'
+   end
+  end
+  
+  def confirm
+    @tweet = Tweet.new(tweets_params)  
+    render :new if @tweet.invalid?
+  end
+  
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to root_path ,notice:"削除しました！"
+  end
+  
+  private
+  def tweets_params
+    params.require(:tweet).permit(:content)
+  end
+  
+  def set_blog
+    @tweets = Tweet.find(params[:id])
+  end  
+  
+end
